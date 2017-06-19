41cdefb42da0504dcf84ffff72fc49e6537f4c6e
Ariel Tapia <arieltapia16@gmail.com>
2017-06-18 21:42:59 -0300

    ...

--- index.android.js
+++ index.android.js
@@ -1,1 +1,15 @@
 //   Index.android.js - Place code in here for Android
+
+//  Import a library to help us create a Component
+
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Create a component
+
+const App = () => (
+  <Text>Some Text</Text>
+);
+
+//  Render it to device
+AppRegistry.registerComponent('albums', () => App);
