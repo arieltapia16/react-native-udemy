0ad56763efa71312df6db28b04e6ad293165365c
Ariel Tapia <arieltapia16@gmail.com>
2017-06-20 21:05:51 -0300

    Album details

--- index.android.js
+++ index.android.js
@@ -3,12 +3,16 @@
 //  Import a library to help us create a Component
 
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
-
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 // Create a component
 
 const App = () => (
-  <Text>Some Text</Text>
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 //  Render it to device
